3418156dcfa97552bbeaa8690e990ddf730db534
enewen0324 <shift9969.cs07@nctu.edu.tw>
2021-01-09 00:28:45 +0800

    Add unit test in question one

--- reverse_string.py
+++ reverse_string.py
@@ -1,3 +1,14 @@
+import unittest
+class test_reverse(unittest.TestCase):
+    def setUp(self):
+        self.args = ("nba cchat gossiping")
+    def test_reverse_no_split(self):
+        ans = "gnipissog tahcc abn"
+        self.assertEqual(ans,reverse(self.args))
+    def test_reverse_split(self):
+        ans = "abn tahcc gnipissog"
+        self.assertEqual(ans,reverse_split(self.args))
+        
 def reverse(str):
     return str[::-1]
 def reverse_split(str):
@@ -10,7 +21,8 @@ def reverse_split(str):
     return str
 
 if __name__ == '__main__':
-    str = input('input: ')
-    print(reverse(str))
-    print(reverse_split(str))
+    unittest.main()
+    # str = input('input: ')
+    # print(reverse(str))
+    # print(reverse_split(str))
     
